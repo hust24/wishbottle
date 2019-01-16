0a2751844da1f2e0ad6c5bc7c3541da10b4c3126
shanxingang <873285083@qq.com>
2019-01-16 11:41:18 +0800

    修改用户接口

--- web/src/main/java/com/hust24/wishbottle/service/UserService.java
+++ web/src/main/java/com/hust24/wishbottle/service/UserService.java
@@ -1,4 +1,12 @@
 package com.hust24.wishbottle.service;
 
+import com.hust24.wishbottle.entity.User;
+
 public interface UserService {
+
+    // 添加新用户
+    User addUser(User user);
+
+    // 修改用户信息
+    User alterUser(User user);
 }
