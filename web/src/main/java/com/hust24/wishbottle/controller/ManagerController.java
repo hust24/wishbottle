346b64bf3a5b566af5ccf406c3f6d5f02bb5feda
章琦 <784510090@qq.com>
2019-01-16 09:20:26 +0800

    导入包

--- web/src/main/java/com/hust24/wishbottle/controller/ManagerController.java
+++ web/src/main/java/com/hust24/wishbottle/controller/ManagerController.java
@@ -3,6 +3,7 @@ package com.hust24.wishbottle.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.hust24.wishbottle.service.ManagerService;
 
 import javax.annotation.Resource;
 
